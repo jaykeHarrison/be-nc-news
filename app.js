0ffca867ca3c383fb5e44a1d596caaf9b0d87ed4
pootleflump <jayke@jaykeharrison.co.uk>
2022-05-16 15:25:39 +0100

    remove accidental auto imported require

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const { getTopics } = require("./controllers/topics.controller.js");
 const express = require("express");
-const res = require("express/lib/response");
 const app = express();
 
 app.use(express.json());
