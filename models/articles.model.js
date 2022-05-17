e650af42ed68b31150b2130990c2f758027cb54f
pootleflump <jayke@jaykeharrison.co.uk>
2022-05-17 16:08:16 +0100

    add GET /api/articles API using MVC and test suite

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,5 +1,19 @@
 const db = require("../db/connection.js");
 
+exports.fetchArticles = () => {
+  return db
+    .query(
+      `SELECT articles.*, COUNT(comments.article_id)::INT AS comment_count
+  FROM articles
+  LEFT JOIN comments ON articles.article_id = comments.article_id
+  GROUP BY articles.article_id
+  ORDER BY articles.created_at DESC;`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
+
 exports.fetchArticleById = (articleId) => {
   return db
     .query(
