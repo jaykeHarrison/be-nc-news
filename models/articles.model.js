71dd309bca79e09512202c1d69a103a416098bd6
pootleflump <jayke@jaykeharrison.co.uk>
2022-05-17 14:32:23 +0100

    add comment_count property to reponse obj of GET /api/articles/article_id

--- models/articles.model.js
+++ models/articles.model.js
@@ -2,7 +2,14 @@ const db = require("../db/connection.js");
 
 exports.fetchArticleById = (articleId) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [articleId])
+    .query(
+      `SELECT articles.*, COUNT(comments.article_id)::INT AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id;`,
+      [articleId]
+    )
     .then((response) => {
       if (response.rows.length === 0) {
         return Promise.reject({
